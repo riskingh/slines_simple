86147c3ec5172d3d21484d7e9d7fad9f864ca0c5
Dima Galov <dimagalov@gmail.com>
2014-05-20 14:44:05 +0400

    snake.js renewed

--- game/snake.js
+++ game/snake.js
@@ -1,57 +1,81 @@
-var mrand = function (a, b) {
-    return Math.floor(Math.random() * (b - a) + a)
-}
+/**
+ * Declaration of Snake, the main structure we will operate with
+ */
+var Snake = function(canvas, color, canvas_color) {
 
-POS_MARGIN = 100
+    /* Canvas attributes */
+    this.canvas        = canvas;
+    this.canvas_width  = parseInt(canvas.prop("width"), 10);
+    this.canvas_height = parseInt(canvas.prop("height"), 10);
+    this.canvas_color  = canvas_color;
 
-var Snake = function (canvas, color, ucolor) {
-    this.canvas = canvas;
-    this.color = color;
-    this.ucolor = ucolor;
-    this.pos = new Point(mrand(0 + POS_MARGIN, parseInt(canvas.prop("width")) - POS_MARGIN), mrand(0 + POS_MARGIN, parseInt(canvas.prop("height")) - POS_MARGIN));
-    this.angle = Math.random() * Math.PI * 2;
-    this.points = [this.pos];
+    /* Initaial values of Snake's attributes */
+    this.color    = color;                                                // color of the snake
+    this.position = new Point(randNumber(100, this.canvas_width - 100),
+                              randNumber(100, this.canvas_height - 100)); // initial position of the snake
+    this.angle    = Math.random() * Math.PI * 2;                          // initial direction of movement
+    this.points   = [this.position];                                      // array of all Snake's points
 
-    this.cangle = 0.1;
-    this.cspeed = 3.5;
-    this.choleprob = 0.08;
-    this.chole = 0;
-    this.cholebord = -10;
-    this.choleminsize = 5;
-    this.cholemaxsize = 10;
+    /*  Motion Constants  */
+    this.cangle       = 0.1;  // rotation angle
+    this.cspeed       = 3.5;  // motion speed
+    this.choleprob    = 0.08; // probability of new hole to appear
+    this.chole        = 0;    // length of the current hole (can be negative, if the Snake is tangible now)
+    this.choleborder  = -10;  // if this.chole < this.choleborder, then we can create new hole
+    this.choleminsize = 5;    // minimal hole size
+    this.cholemaxsize = 10;   // maximal hole size
 
-    this.update = function (dir) {
-        this.chole--;
+    /**
+    * Updates Snake's state according to the current movement direction
+    */
+    this.update = function (direction) {
+        // we don't won't to draw a hole by default
+        --this.chole;
 
-        prev = this.pos;
-        this.angle += dir * this.cangle;
+        // memorize the previous position
+        previous = this.position;
+
+        // calculate new angle according to current direction
+        this.angle += direction * this.cangle;
+
+        // angle should be in [0; 2 * pi] range
         if (this.angle < 0)
             this.angle += 2 * Math.PI;
         if (this.angle >= 2 * Math.PI)
             this.angle -= 2 * Math.PI;
-        this.pos = move(this.pos, turn(new Vector(0, this.cspeed), this.angle));
 
-        if (this.chole > 0 || (this.chole < this.cholebord && Math.random() < this.choleprob)) {
+        // calculate new position
+        this.position = shiftPoint(this.position, rotateVector(new Vector(0, this.cspeed), this.angle));
+
+        // if we are in a hole now or we want to create one and random allows us to do so
+        if (this.chole > 0 || (this.chole < this.choleborder && Math.random() < this.choleprob)) {
+            // then we should add new fakePoint to our snake
             this.points.push(new fakePoint());
-            if (this.chole < this.cholebord)
-                this.chole = mrand(this.choleminsize, this.cholemaxsize + 1);
+
+            // if we created a new hole, then we should choose it's length
+            if (this.chole < this.choleborder) {
+                this.chole = randNumber(this.choleminsize, this.cholemaxsize + 1);
+            }
+
+            // draw invisible the part of the Snake from previous position to current
             this.canvas.drawLine({
-                strokeStyle: this.ucolor,
-                strokeWidth: 5,
-                rounded: true,
-                x1: prev.x, y1: prev.y,
-                x2: this.pos.x, y2: this.pos.y
+                strokeStyle: this.canvas_color,          // the hole is invisible, so the colour is equal to the canvas colour
+                strokeWidth: 5,                          // width of the Snake
+                rounded: true,                           // our Snake should look pretty
+                x1: previous.x, y1: previous.y,          // one end of the segment
+                x2: this.position.x, y2: this.position.y // other end of the segment
             });
         }
         else {
-            this.points.push(this.pos);
+            // draw visible the part of the Snake from previous position to current
+            this.points.push(this.position);
             this.canvas.drawLine({
-                strokeStyle: this.color,
-                strokeWidth: 5,
-                rounded: true,
-                x1: prev.x, y1: prev.y,
-                x2: this.pos.x, y2: this.pos.y
+                strokeStyle: this.color,                 // snake is visible, so the colour is equal to the Snake colour
+                strokeWidth: 5,                          // width of the Snake
+                rounded: true,                           // our Snake should look pretty
+                x1: previous.x, y1: previous.y,          // one end of the segment
+                x2: this.position.x, y2: this.position.y // other end of the segment
             });
         }
-    }
-}
+    };
+};
